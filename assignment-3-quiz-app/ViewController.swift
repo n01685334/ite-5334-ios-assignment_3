0edcb8fe9f689509323f8875cce08b48d372b2f3
James Chard N01685334 <n01685334@humber.ca>
2025-03-27 22:17:21 -0400

    Initial Commit

---
+++ assignment-3-quiz-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment-3-quiz-app
+//
+//  Created by James Chard on 2025/3/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
